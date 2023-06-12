3d9dcb27f892a9460255fdbe0596a18eb0c8e6c9
KotaSC <is0302ke@gmail.com>
2023-06-12 22:39:04 +0900

    feat: add button component P.39

--- code-stages/chapter-03-start/src/components/button/button.tsx
+++ code-stages/chapter-03-start/src/components/button/button.tsx
@@ -1,6 +1,51 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type ButtonProps = {};
+import React, {
+  MouseEventHandler,
+  ReactNode,
+} from 'react';
+import { Button as ChakraButton } from '@chakra-ui/react';
 
-export const Button = () => {
-  return <>Button</>;
+const variants = {
+  solid: {
+    variant: 'solid',
+    bg: 'primary',
+    color: 'primaryAccent',
+    _hover: {
+      opacity: 0.9,
+    },
+  },
+  outline: {
+    variant: 'outline',
+    bg: 'white',
+    color: 'primary',
+  },
+};
+
+export type ButtonProps = {
+  children: ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+  variant?: keyof typeof variants;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  icon?: JSX.Element;
+};
+
+export const Button = ({
+  children,
+  variant = 'solid',
+  type = 'button',
+  icon,
+  ...props
+}: ButtonProps) => {
+  return (
+    <ChakraButton
+      {...props}
+      {...variants[variant]}
+      type={type}
+      leftIcon={icon}
+    >
+      {children}
+    </ChakraButton>
+  );
 };
--- code-stages/chapter-03-start/src/pages/index.tsx
+++ code-stages/chapter-03-start/src/pages/index.tsx
@@ -1,5 +1,20 @@
+import React from 'react';
+import { Button } from '@/components/button/button';
+import { InputField } from '@/components/form';
+import { Link } from '@/components/link';
+
 const LandingPage = () => {
-  return <>Landing Page</>;
+  return (
+    <>
+      <Button variant="solid" type="button">
+        Click me
+      </Button>
+      <br />
+      <InputField />
+      <br />
+      <Link />
+    </>
+  );
 };
 
 export default LandingPage;
