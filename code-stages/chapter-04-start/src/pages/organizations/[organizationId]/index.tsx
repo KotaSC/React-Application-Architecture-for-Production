72b04d3cea665598579813fb4c803a94636027c0
KotaSC <is0302ke@gmail.com>
2023-06-21 19:47:02 +0900

    feat: add organizationpage

--- code-stages/chapter-04-start/src/pages/organizations/[organizationId]/index.tsx
+++ code-stages/chapter-04-start/src/pages/organizations/[organizationId]/index.tsx
@@ -7,9 +7,9 @@ import React, { ReactElement } from 'react';
 
 import { NotFound } from '@/components/not-found';
 import { Seo } from '@/components/seo';
-import { JobList, Job } from '@/components/features/jobs';
+import { JobsList, Job } from '@/features/jobs';
 import { OrganizationInfo } from '@/features/organizations';
-import { PublicLayout } from '@/components/layouts/public-layout';
+import { PublicLayout } from '@/layouts/public-layout';
 import {
   getJobs,
   getOrganization,
@@ -41,7 +41,7 @@ const PublicOrganizationPage = ({
         <Heading size="md" my="6">
           Open Jobs
         </Heading>
-        <JobList
+        <JobsList
           jobs={jobs}
           organizationId={organization.id}
           type="public"
@@ -49,4 +49,35 @@ const PublicOrganizationPage = ({
       </Stack>
     </>
   );
+};
+
+PublicOrganizationPage.getLayout = function getLayout(
+  page: ReactElement
+) {
+  return <PublicLayout>{page}</PublicLayout>;
+};
+
+export default PublicOrganizationPage;
+
+/**
+ * paramはURLのパラメーターを取得するためのもので，
+ * このページの場合はorganizationIdを取得するために使用しています。
+ * 例えば，/organizations/123の場合，paramsは{ organizationId: '123' }となります。
+ */
+export const getServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => {
+  const organizationId = params?.organizationId as string;
+
+  const [organization, jobs] = await Promise.all([
+    getOrganization(organizationId).catch(() => null),
+    getJobs(organizationId).catch(() => [] as Job[]),
+  ]);
+
+  return {
+    props: {
+      organization,
+      jobs,
+    },
+  };
 };
