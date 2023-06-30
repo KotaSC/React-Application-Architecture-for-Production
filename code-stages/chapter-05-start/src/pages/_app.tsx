bb47921e4ab62c329437446f1e1354779e99386a
KotaSC <is0302ke@gmail.com>
2023-06-30 23:00:58 +0900

    feat: API mocking

--- code-stages/chapter-05-start/src/pages/_app.tsx
+++ code-stages/chapter-05-start/src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import type { AppProps } from 'next/app';
-import { ReactElement, ReactNode } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 
 import { AppProvider } from '@/providers/app';
+import { API_MOCKING } from '@/config/constants';
+import { MSWWrapperProps } from '@/lib/msw';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -11,6 +14,10 @@ type NextPageWithLayout = NextPage & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+
+const MSWWrapper = dynamic<MSWWrapperProps>(() =>
+  import('@/lib/msw').then(({ MSWWrapper }) => MSWWrapper)
+);
 
 const App = ({
   Component,
@@ -23,7 +30,15 @@ const App = ({
     <Component {...pageProps} />
   );
 
-  return <AppProvider>{pageContent}</AppProvider>;
+  return (
+    <AppProvider>
+      {API_MOCKING ? (
+        <MSWWrapper>{pageContent}</MSWWrapper>
+      ) : (
+        pageContent
+      )}
+    </AppProvider>
+  );
 };
 
 export default App;
--- code-stages/chapter-05-start/src/testing/mocks/seed-db.ts
+++ code-stages/chapter-05-start/src/testing/mocks/seed-db.ts
@@ -1,1 +1,16 @@
-export const seedDb = () => {};
+import { db } from './db';
+import { testData } from '../test-data';
+
+export const seedDb = () => {
+  const userCount = db.user.count();
+
+  if (userCount > 0) return;
+
+  testData.users.forEach((user) => db.user.create(user));
+
+  testData.organizations.forEach((organization) =>
+    db.organization.create(organization)
+  );
+
+  testData.jobs.forEach((job) => db.job.create(job));
+};
