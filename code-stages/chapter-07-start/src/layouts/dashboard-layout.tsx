0d9d4f155aa96fbfdd10bae93693def8e6912216
KotaSC <is0302ke@gmail.com>
2023-07-15 21:23:07 +0900

    feat: add logout api

--- code-stages/chapter-07-start/src/layouts/dashboard-layout.tsx
+++ code-stages/chapter-07-start/src/layouts/dashboard-layout.tsx
@@ -5,11 +5,14 @@ import {
   Flex,
   HStack,
 } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
 import { Button } from '@/components/button';
 import { Link } from '@/components/link';
 import { useUser } from '@/testing/test-data';
+
+import { useRouter } from 'next/router';
+import { useLogout } from '@/features/auth';
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -38,6 +41,10 @@ export const DashboardLayout = ({
 };
 
 const Navbar = () => {
+  const router = useRouter();
+  const logout = useLogout({
+    onSuccess: () => router.push('/auth/login'),
+  });
   return (
     <Box as="nav" bg="primary" color="primaryAccent">
       <Container maxW="container.lg" size="3xl" py="3">
@@ -58,10 +65,10 @@ const Navbar = () => {
           </HStack>
           <HStack>
             <Button
+              isDisabled={logout.isLoading}
+              isLoading={logout.isLoading}
               variant="outline"
-              onClick={() =>
-                console.log('Logging Out...')
-              }
+              onClick={() => logout.submit()}
             >
               Log Out
             </Button>
